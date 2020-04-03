896c068c4a724977172058e93946fb3354ec19ca
Maxwell Elliott <maxwell.elliott@gotinder.com>
2020-04-03 12:35:51 -0700

    demo

--- BazeliOSLab/bazel_smart_ci.sh
+++ BazeliOSLab/bazel_smart_ci.sh
@@ -1,7 +1,4 @@
-COMMIT_RANGE=${COMMIT_RANGE:-$(git merge-base origin/master HEAD)".."}
-
-# Go to the root of the repo
-cd "$(git rev-parse --show-toplevel)"
+COMMIT_RANGE=${COMMIT_RANGE:-$(git merge-base master HEAD)".."}
 
 # Get a list of the current files in package form by querying Bazel.
 files=()
@@ -17,5 +14,5 @@ tests=$(bazel query \
 # Run the tests if there were results
 if [[ ! -z $tests ]]; then
   echo "Running tests"
-  bazel test $tests
+  bazel test $tests --apple_platform_type=ios --ios_minimum_os=13.0 --ios_simulator_device="iPhone 8" --ios_simulator_version=13.3
 fi
