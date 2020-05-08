8a0b9ca0f1de47b37cceaa99dd6ffdc44acd5eca
Maxwell Elliott <maxwell.elliott@gotinder.com>
2020-05-08 15:48:22 -0700

    Commit modules

---
+++ BazeliOSLab/Modules/YgfwnzhkjdqjkhpijrvyModule/Tests/YgfwnzhkjdqjkhpijrvyClassTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+import Foundation
+@testable import YgfwnzhkjdqjkhpijrvyModule
+
+class YgfwnzhkjdqjkhpijrvyModuleTests: XCTestCase {
+    func testExample() {
+        let obj =  YgfwnzhkjdqjkhpijrvyClass()
+        XCTAssertEqual(obj.getUUID(), "Ygfwnzhkjdqjkhpijrvy")
+    }
+}
